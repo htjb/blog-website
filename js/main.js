06fd46dba6afbdeb627cde8daecd99ca473a12cb
htjb <harrytjbevins@yahoo.co.uk>
2025-10-07 22:28:02 +0100

    moving post parsing and welcome loading to utils

--- js/main.js
+++ js/main.js
@@ -1,60 +1,8 @@
-import { loadMd } from "./utils.js";
+import { parsePost, loadWelcome, cleanHtml } from "./utils.js";
 
-async function parsePost(elementId, postName){
-    let outputDiv = document.createElement("div");
-    outputDiv.setAttribute("id", elementId);
-    outputDiv.setAttribute("class", "card");
-    textContents.appendChild(outputDiv);
-    let post = await loadMd(elementId, postName)
-    // first 4 lines are metadata
-    let metaInfoText = post.split('\n').slice(0, 4)
-    let metaInfo = {}
-    for (let meta of metaInfoText){
-        if (meta.split(':').length == 2){
-            metaInfo[meta.split(':')[0]] = meta.split(':')[1]
-        }
-    }
+loadWelcome()
 
-    // everything after the first 5 lines is the post content
-    let postHTML = marked.parse(
-        post.split('\n').slice(5, post.length).join('\n'), 
-        {breaks: true})
-
-    let postHTMLList = postHTML.split('\n')
-    let title = postHTMLList.shift()
-    title = '<h1>' + metaInfo['date'] + ' - ' +  title.slice(4, title.length)
-    title = '<div class="card-header">' + title + '\n' + 
-        //metaInfo['tags'].split(',').map(tag => '<a href="" class="' + tag + '">' + tag.trim() + 
-        //'</a>').join(',') +
-        '</div>'
-    postHTML = title + '<div class="card-body">\n' + postHTMLList.join('\n') + 
-        //'<p align="center"> Tags:' +
-        //metaInfo['tags'].split(',').map(tag => ' ' + tag.trim() + ' ').join('')
-        //+ '</p>'
-        '\n</div>'
-    document.getElementById(elementId).innerHTML = postHTML
-    renderMathInElement(document.getElementById(elementId), {
-        delimiters: [
-      {left: '$$', right: '$$', display: true},
-      {left: '$',  right: '$',  display: false}
-    ]
-    }); // KaTeX
-    // find all the code blocks inside pre blocks inside the post and highlight them
-    document.querySelectorAll(`#${elementId} pre code`).forEach((block) => {
-        hljs.highlightElement(block);
-    });
-}
-
-// tag for text content
 let textContents = document.getElementById("text-content")
-// load in the welcome message
-let welcomeDiv = document.createElement("div");
-welcomeDiv.setAttribute("id", "welcome-div")
-textContents.appendChild(welcomeDiv)
-let welcome = await loadMd('welcome-div', 'includes/welcome.md')    
-let welcomeHTML = marked.parse(welcome, {breaks: true})
-document.getElementById('welcome-div').innerHTML = welcomeHTML
-renderMathInElement(document.getElementById("welcome-div")); // KaTeX
 
 fetch('posts/tag-list.txt')
     .then(response => response.text())
@@ -96,24 +44,19 @@ fetch('posts/tag-list.txt')
         });
     });
 
-/*document.addEventListener('click', (e) => {
-  if (e.target.matches('.tag-button')) {
-    console.log('e.innerText', e.target.innerText);
-  }
-  let cards = document.querySelectorAll('.card');
-  // want to show all cards if no tag selected otherwise filter
-  if (e.target.matches('.tag-button')){
-    let selectedTag = e.target.innerText;
-    cards.forEach(card => {
-        if (card.innerHTML.includes(selectedTag)){
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-  } else {
-    cards.forEach(card => {
-        card.style.display = 'block';
-    });
-  }
-});*/
+// document.addEventListener('click', (e) => {
+//   cleanHtml(textContents)
+//   loadWelcome()
+//   fetch('posts/post-list.txt')
+//     .then(response => response.text())
+//     .then(data => {
+//         let postList = data.split('\n').filter(line => line.length > 0);
+//         for (let i = postList.length - 1; i >= 0; i--){
+//             let postId = 'post' + (i + 1);
+//             parsePost(postId, postList[i]);
+//         }
+//         document.querySelectorAll('.card').forEach(card => {
+//         card.addEventListener('click', () => card.classList.toggle('open'));
+//         });
+//     });
+// });
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,3 @@
-export function loadHTML(elementId, filePath) {
-  fetch(filePath)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById(elementId).innerHTML = html;
-    });
-}
-
 export async function loadMd(elementId, filePath) {
   let r = fetch(filePath).then(r => r.text())
   return r
@@ -13,4 +5,63 @@ export async function loadMd(elementId, filePath) {
 
 export async function cleanHtml(id) {
   document.getElementById(id).innerHTML = "";
+}
+
+export async function parsePost(elementId, postName, tag=null){
+    let textContents = document.getElementById("text-content")
+    let outputDiv = document.createElement("div");
+    outputDiv.setAttribute("id", elementId);
+    outputDiv.setAttribute("class", "card");
+    textContents.appendChild(outputDiv);
+    let post = await loadMd(elementId, postName)
+    // first 4 lines are metadata
+    let metaInfoText = post.split('\n').slice(0, 4)
+    let metaInfo = {}
+    for (let meta of metaInfoText){
+        if (meta.split(':').length == 2){
+            metaInfo[meta.split(':')[0]] = meta.split(':')[1]
+        }
+    }
+
+    // everything after the first 5 lines is the post content
+    let postHTML = marked.parse(
+        post.split('\n').slice(5, post.length).join('\n'), 
+        {breaks: true})
+
+    let postHTMLList = postHTML.split('\n')
+    let title = postHTMLList.shift()
+    title = '<h1>' + metaInfo['date'] + ' - ' +  title.slice(4, title.length)
+    title = '<div class="card-header">' + title + '\n' + 
+        //metaInfo['tags'].split(',').map(tag => '<a href="" class="' + tag + '">' + tag.trim() + 
+        //'</a>').join(',') +
+        '</div>'
+    postHTML = title + '<div class="card-body">\n' + postHTMLList.join('\n') + 
+        //'<p align="center"> Tags:' +
+        //metaInfo['tags'].split(',').map(tag => ' ' + tag.trim() + ' ').join('')
+        //+ '</p>'
+        '\n</div>'
+    document.getElementById(elementId).innerHTML = postHTML
+    renderMathInElement(document.getElementById(elementId), {
+        delimiters: [
+      {left: '$$', right: '$$', display: true},
+      {left: '$',  right: '$',  display: false}
+    ]
+    }); // KaTeX
+    // find all the code blocks inside pre blocks inside the post and highlight them
+    document.querySelectorAll(`#${elementId} pre code`).forEach((block) => {
+        hljs.highlightElement(block);
+    });
+}
+
+export async function loadWelcome(){
+  // tag for text content
+  let textContents = document.getElementById("text-content")
+  // load in the welcome message
+  let welcomeDiv = document.createElement("div");
+  welcomeDiv.setAttribute("id", "welcome-div")
+  textContents.appendChild(welcomeDiv)
+  let welcome = await loadMd('welcome-div', 'includes/welcome.md')    
+  let welcomeHTML = marked.parse(welcome, {breaks: true})
+  document.getElementById('welcome-div').innerHTML = welcomeHTML
+  renderMathInElement(document.getElementById("welcome-div")); // KaTeX
 }
