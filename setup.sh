9ba38a2a7a72d10572158f3e326d2c1c40a87154
htjb <harrytjbevins@yahoo.co.uk>
2026-03-09 17:40:30 +0000

    make commit hook executable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Run once after cloning to enable the pre-commit hook that auto-generates README.md.
+# Run once after cloning to enable the pre-commit hook.
+chmod +x .githooks/pre-commit
 git config core.hooksPath .githooks
 echo "Git hooks configured. post and tag lists will be regenerated automatically on each commit."
